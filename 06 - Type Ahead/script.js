0940e76de973c058e14261a333184305c0ef2c14
georgina puig <georginaxcodes@gmail.com>
2020-08-13 20:06:12 -0500

    find matches

--- 06 - Type Ahead/script.js
+++ 06 - Type Ahead/script.js
@@ -1,1 +1,16 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data))
+  .then(data => console.log(cities[0].city));
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // if the city or statr matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
