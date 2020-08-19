bc85ff4852fea38db41b1cba9219c0e54d4ecb2b
georgina puig <georginaxcodes@gmail.com>
2020-08-18 23:55:47 -0500

    shiftKey checked

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,4 +1,27 @@
 const checkbox = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-const handleCheck = e => console.log(e);
+let lastChecked;
+
+function handleCheck(e) {
+  // check if they had the shift key down && check that they are checking it
+  let inBetween = false;
+  
+  if (e.shiftKey && this.checked) {
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
