fab2fb170854716d3c20434647fb6fd3daf37389
georgina puig <georginaxcodes@gmail.com>
2020-08-24 13:37:56 -0500

    get elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
