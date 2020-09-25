e735cf0a6ab264c1ef6b53b64fbd8c33b8aac6aa
georgina puig <georginaxcodes@gmail.com>
2020-09-24 23:58:29 -0500

    slider images and check slide

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -12,3 +12,11 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slider-in');
+
+function checkSlide(e) {
+  console.count(e);
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
