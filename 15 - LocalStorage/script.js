42611ccd2bf190c5dc7eba0ca338f41a6111e354
georgina puig <georginaxcodes@gmail.com>
2020-10-31 23:14:58 -0600

    add button event listener

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,3 +1,9 @@
-  const addItems = document.querySelector('.add-items');
-  const itemsList = document.querySelector('.plates');
-  const items = [];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+  e.preventDefault(); // stop the page from reloading
+}
+
+addItems.addEventListener('submit', addItem);
