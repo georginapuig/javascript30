7fafde6965ada9f14ebcc0ea5aa2c94a10d26c3f
georgina puig <georginaxcodes@gmail.com>
2020-11-12 20:48:27 -0600

    add text and item

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -4,6 +4,15 @@ const items = [];
 
 function addItem(e) {
   e.preventDefault(); // stop the page from reloading
+
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text, // same as text: text
+    done: false 
+  };
+
+  items.push(item);
+  this.reset();
 }
 
 addItems.addEventListener('submit', addItem);
