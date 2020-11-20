189ef9ef0176bdd60fec89f82f6e013387884407
georgina puig <georginaxcodes@gmail.com>
2020-11-20 00:10:10 -0600

    event delegation checkbox

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -28,6 +28,16 @@ function populateList(platesList, plates = []) {
   }).join('');
 }
 
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
