222a044f5c4f660d4bf72711780b2e168ae8087a
georgina puig <georginaxcodes@gmail.com>
2020-12-27 23:08:40 -0600

    add walk

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 100; // 100px
 
 function shadow(e) {
   // const width = hero.offsetWidth;
@@ -11,7 +12,17 @@ function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero; // same as line 5 and 6
   let { offsetX: x, offsetY: y } = e;
   
-  console.log(x, y);
+  // console.log(x, y);
+  // console.log(this,  e.target);
+  console.log(x,  width);
+  
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / width * walk) - (walk / 2));
 }
 
 hero.addEventListener('mousemove', shadow);
