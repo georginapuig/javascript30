263e71264a5f4299c09e7d4d9c3bf2a316167e1b
georgina puig <georginaxcodes@gmail.com>
2020-12-28 23:19:27 -0600

    finish day 16

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -14,15 +14,25 @@ function shadow(e) {
   
   // console.log(x, y);
   // console.log(this,  e.target);
-  console.log(x,  width);
   
   if (this !== e.target) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
-
+  
+  console.log(x,  width);
+  
   const xWalk = Math.round((x / width * walk) - (walk / 2));
-  const yWalk = Math.round((y / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,255,0,0.7)`;
+
+  // text.style.textShadow = `
+  //   ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+  //   ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+  //   ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+  //   ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+  // `;
 }
 
 hero.addEventListener('mousemove', shadow);
